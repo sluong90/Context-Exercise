579fc883db1460c88e6356de5f321bd0e8e635cc
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2019-01-21 20:29:58 -1000

    completed exercise

--- context.js
+++ context.js
@@ -8,6 +8,8 @@ function add ( op1, op2 ) {
   return this.name + " can count to " + (op1 + op2);
 }
 
+
+
 var voltron = new Robot("Voltron");
 var bender = new Robot("Bender Rodriguez");
 var optimus = new Robot("Optimus Prime");
@@ -17,14 +19,14 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add(0,1));
 
 // #2  
-console.log(  );
+console.log(add.call(voltron,2,3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus,[20,30]));
 
 // #4
-
-console.log(  );
+var calculate = add.bind(bender)
+console.log(calculate('drinking ', 'beer'));
